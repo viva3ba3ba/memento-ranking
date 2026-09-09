cb0c1cfa34e9c04bc7112451a92d75e818f9ddd1
viva3ba3ba <viva3ba3ba@gmail.com>
2026-09-10 00:00:04 +0900

    公開ランキング v59: dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -262,7 +262,7 @@ if (isExtension)
   });
 else if ("serviceWorker" in navigator)
   navigator.serviceWorker
-    .register("./sw.js?v=58", { updateViaCache: "none" })
+    .register("./sw.js?v=59", { updateViaCache: "none" })
     .then((reg) => reg.update())
     .catch(() => {});
 load();
