54eec7d329532f8a1855c5b5c61a54da63b319b3
viva3ba3ba <viva3ba3ba@gmail.com>
2026-09-24 21:53:36 +0900

    公開ランキング v108: pwa.js

--- pwa.js
+++ pwa.js
@@ -4,7 +4,7 @@ if (
 ) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./sw.js?v=69", { updateViaCache: "none" })
+      .register("./sw.js?v=70", { updateViaCache: "none" })
       .then((registration) => registration.update())
       .catch(() => {});
   });
